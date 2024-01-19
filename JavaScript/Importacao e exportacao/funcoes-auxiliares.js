3cf3007a6952fa0bdf9e0eea2ae3f859c594d6b6
Thayna Lima <cristinathayna06@gmail.com>
2024-01-19 09:42:49 -0300

    Finalização importação e exportação

--- JavaScript/Importacao e exportacao/funcoes-auxiliares.js
+++ JavaScript/Importacao e exportacao/funcoes-auxiliares.js
@@ -1,9 +1,14 @@
-function gets(params) {
-    return 20;
+const entradas = [5, 50, 10, 98, 23];
+let contador = 0;
+
+function gets() {
+    const entradaAtual = entradas[contador];
+    contador++;
+    return entradaAtual;
 }
 
-function print( parametro ) {
-    console.log(parametro);
+function print( texto ) {
+    console.log(texto);
 }
 
 module.exports = {gets, print};
--- JavaScript/Importacao e exportacao/main.js
+++ JavaScript/Importacao e exportacao/main.js
@@ -1,6 +1,14 @@
-const { gets, print } = require('./funcoes-auxiliares.js');
-print(gets());
-
 /*const pessoa = { nome: 'Bruna' };
 
 const { nome } = pessoa; //ou const nome = pessoa.nome; */
+const { gets, print } = require('./funcoes-auxiliares.js');
+let maiorNum = 0;
+const quantidadeLista = gets();
+
+for (let i = 0; i < quantidadeLista; i++) {
+    const numSorteado = gets();
+    if (numSorteado > maiorNum) {
+        maiorNum = numSorteado;
+    }
+}
+console.log(maiorNum);
